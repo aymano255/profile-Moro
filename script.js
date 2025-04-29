c215c248f81bda149a7273d449d1315cca0b4ace
aymano255 <123076561+aymano255@users.noreply.github.com>
2025-04-29 09:38:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-document.addEventListener('keydown', function(event) {
-    const key = event.key.toLowerCase();
+document.addEventListener('click', function () {
     const video = document.getElementById('bgVideo');
     const intro = document.getElementById('intro');
     const main = document.getElementById('main');
 
-    if (key === 'y') {
-        intro.style.display = 'none';
-        main.style.display = 'flex';
-        video.muted = false;
-        video.volume = 0.3; // Volumen al 30%
-        video.play();
-    }
-
-    if (key === 'm') {
-        video.muted = !video.muted;
-    }
+    intro.style.display = 'none';
+    main.style.display = 'flex';
+    
+    video.muted = false;
+    video.volume = 0.3; // volumen al 30%
+    video.play();
 });
